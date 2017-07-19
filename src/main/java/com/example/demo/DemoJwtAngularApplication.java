8036fc76be8f8d42a96017e5092a1dcd9417650d
Andreas Capri <capriandreas@gmail.com>
2017-07-19 14:06:09 +0700

    First Commit Simple JWT

---
+++ src/main/java/com/example/demo/DemoJwtAngularApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+public class DemoJwtAngularApplication {
+	
+	@Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/*");
+
+        return registrationBean;
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtAngularApplication.class, args);
+	}
+}
